d26a12b261556200ff8a9ecdc3affa9478f141b4
Fin Carter <fcarter@unca.edu>
2020-10-07 18:53:03 -0400

    view fixes

--- src/StringView.java
+++ src/StringView.java
@@ -1,8 +1,10 @@
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionListener;
 import javax.swing.*;
 
 public class StringView extends JFrame {
-
+	
 	private JTextField firstString = new JTextField(10);
 	private JLabel addition = new JLabel("+");
 	private JTextField secondString = new JTextField(10);
@@ -18,26 +20,53 @@ public class StringView extends JFrame {
 	private JTextField pOut = new JTextField(10);
 
 	StringView() {
-		JPanel mainView = new JPanel();
+		JTabbedPane TabbedPane = new JTabbedPane();
+	    JPanel PanelFirst = new JPanel();
+	    JPanel PanelSecond = new JPanel();
+	    JPanel PanelThird = new JPanel();
+	    
+	    //////////////
+	    //FIRST TAB//
+	    //////////////
+	    PanelFirst.setLayout(null);
+	    TabbedPane.addTab("Combine", PanelFirst);
+	    firstString.setBounds(20,20,150,23);
+	    addition.setBounds(200,20,60,23);
+	    secondString.setBounds(250,20,150,23);
+	    combineButton.setBounds(20,50,150,23);
+	    combinedOutputDisplay.setBounds(20,80,150,23);
+		PanelFirst.add(firstString);
+		PanelFirst.add(addition);
+	    PanelFirst.add(secondString);
+	    PanelFirst.add(combineButton);
+	    PanelFirst.add(combinedOutputDisplay);
+	    //////////////
+	    //SECOND TAB//
+	    //////////////
+	    PanelSecond.setLayout(null);
+	    TabbedPane.addTab("Reverse", PanelSecond);
+	    rString.setBounds(20,20,150,23);
+	    reverseButton.setBounds(200,20,150,23);
+	    reverseDisplay.setBounds(20,50,150,23);
+	    PanelSecond.add(rString);
+	    PanelSecond.add(reverseButton);
+	    PanelSecond.add(reverseDisplay);
+	    //////////////
+	    //THIRD TAB///
+	    //////////////
+	    PanelThird.setLayout(null);
+	    pString.setBounds(20,20,150,23);
+	    pButton.setBounds(200,20,150,23);
+	    pOut.setBounds(20,50,150,23);
+	    PanelThird.add(pString);
+	    PanelThird.add(pButton);
+	    PanelThird.add(pOut);
+	    TabbedPane.addTab("Palindrome", PanelThird);
 
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setSize(500, 200);
-
-		mainView.add(firstString);
-		mainView.add(addition);
-		mainView.add(secondString);
-		mainView.add(combineButton);
-		mainView.add(combinedOutputDisplay);
-
-		mainView.add(rString);
-		mainView.add(reverseButton);
-		mainView.add(reverseDisplay);
 		
-		mainView.add(pString);
-		mainView.add(pButton);
-		mainView.add(pOut);
-
-		this.add(mainView);
+		this.add(TabbedPane);
 	}
 
 	public String getFirstString() {
